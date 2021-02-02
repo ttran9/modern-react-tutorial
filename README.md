bde6f23cedf6558e4189454f53b91d82b7b69aae
ttran9 <toddtran9@gmail.com>
2021-02-02 02:11:24 -0800

    initial commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Dojo Blog
+
+# Some Information
+
+    - I will be following this [a react tutorial](https://www.youtube.com/playlist?list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d).
+    - I am starting this repository up to the end of the third video.
+
+# Video Note(s) (if applicable):
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./index.css";
+
+class App extends Component {
+    render() {
+        return (
+              <div className="App">
+                <div className="content">
+                    <h1>App Component</h1>
+                </div>
+              </div>  
+        )
+
+    }
+
+}
+
+export default App;
