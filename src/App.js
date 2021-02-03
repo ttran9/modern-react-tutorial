46a291178a95effe0af5f7ae8386f19459c39c94
ttran9 <toddtran9@gmail.com>
2021-02-02 19:21:14 -0800

    using variables.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from "react";
 import "./index.css";
 
-class App extends Component {
-    render() {
-        return (
-              <div className="App">
-                <div className="content">
-                    <h1>App Component</h1>
-                </div>
-              </div>  
-        )
+function App() {
+    const title = 'Welcome to the new blog';
+    const likes = 50;
+    // const person = { name: 'yoshi', age: 30};
+    const link = "http://www.google.com";
 
-    }
+    return (
+            <div className="App">
+            <div className="content">
+                <h1>{title}</h1>
+                <p>Liked { likes } times</p>
+                {/* <p>{person}</p> */}
+                <p> {10} </p>
+                <p> {'Hello, Ninjas'} </p>
+                <p> {[1,2,3,4,5]} </p>
+                <p> {Math.random() * 10 } </p>
+
+                <a href={link}>Google Site</a>
+            </div>
+            </div>  
+    )
 
 }
 
