f6cd2f312372f09c6507cad735f3abfd29f5ec1b
ttran9 <toddtran9@gmail.com>
2021-02-02 19:46:29 -0800

    displaying components.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
-import "./index.css";
+import React from "react";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
     const title = 'Welcome to the new blog';
@@ -9,17 +10,10 @@ function App() {
 
     return (
             <div className="App">
-            <div className="content">
-                <h1>{title}</h1>
-                <p>Liked { likes } times</p>
-                {/* <p>{person}</p> */}
-                <p> {10} </p>
-                <p> {'Hello, Ninjas'} </p>
-                <p> {[1,2,3,4,5]} </p>
-                <p> {Math.random() * 10 } </p>
-
-                <a href={link}>Google Site</a>
-            </div>
+                <Navbar/>
+                <div className="content">
+                    <Home/>
+                </div>
             </div>  
     )
 
