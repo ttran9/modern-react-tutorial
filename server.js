f9d32b4b2dd4cf0d937abbee743702ddc13e9f53
ttran9 <toddtran9@gmail.com>
2021-02-03 23:33:40 -0800

    setting up JSON server to run with express.

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const path = require("path");
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('./data/db.json');
+const middlewares = jsonServer.defaults();
 
 const frontend = path.join(__dirname, "dist/");
 
 // map the requests to the static react build directory
 app.use("/", express.static(frontend));
+
+app.use(middlewares);
+app.use(router);
 
 // all the unknown requests are redirected to the react SPA
 app.use(function(req, res, next) {
