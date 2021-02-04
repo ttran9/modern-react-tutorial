45c8978acc35333dd8776e755cbf3659a9b5a6cc
ttran9 <toddtran9@gmail.com>
2021-02-03 21:02:02 -0800

    creating functions to handle events.

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,20 @@
 import React from "react";
 
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log('hello, ninjas', e);
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log('hello ' + name, e.target);
+    }
+
     return ( 
         <div className="home">
             <h1>Home Page</h1>
+            <button onClick={handleClick}>click me</button>
+            <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button>
         </div>
     );
         
