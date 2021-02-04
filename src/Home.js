fc3f44fa56a56fbbb8c3bda59f7bdcd455ca5847
ttran9 <toddtran9@gmail.com>
2021-02-03 21:48:18 -0800

    using useState hook.

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,28 @@
 import React from "react";
+import { useState } from 'react';
 
 const Home = () => {
 
-    const handleClick = (e) => {
-        console.log('hello, ninjas', e);
+    // let name = 'mario';
+    // second is function to be able to change the name
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(25);
+
+    const handleClick = () => {
+        setName('luigi'); // this forces react to re-render the component.
+        setAge(30);
     }
 
-    const handleClickAgain = (name, e) => {
-        console.log('hello ' + name, e.target);
-    }
+    // const handleClickAgain = (name, e) => {
+    //     console.log('hello ' + name, e.target);
+    // }
 
     return ( 
         <div className="home">
             <h1>Home Page</h1>
+            <p>{name } is { age } years old</p>
             <button onClick={handleClick}>click me</button>
-            <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button>
+            {/* <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button> */}
         </div>
     );
         
