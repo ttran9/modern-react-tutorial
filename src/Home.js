577d440422ff45391e2eeab1a3a7d3dd61cc24e6
ttran9 <toddtran9@gmail.com>
2021-02-03 23:14:54 -0800

    demonstrating useEffect dependency.

--- src/Home.js
+++ src/Home.js
@@ -10,6 +10,8 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
       ]);
 
+    const [name, setName] = useState('mario');
+
     const handleDelete = (id) => {
         // the returned blogs returns a new filtered array. 
         const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -19,11 +21,14 @@ const Home = () => {
     /*
      * this function below is run on the initial render and every time there is 
      a re-rendering (such as when the data is changed).
+     * adding the empty array makes it so useEffect is only run once. 
+     * if we want useEffect to run when the name state changes but not when the blogs state changes
+     * then the name becomes a dependency we add into the second argument.
      */
     useEffect(() => {
         console.log('use effect ran');
-        console.log(blogs);
-    });
+        console.log(name);
+    }, [name]);
     // let name = 'mario';
     // second is function to be able to change the name
     // const [name, setName] = useState('mario');
@@ -43,6 +48,8 @@ const Home = () => {
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+            <button onClick={() => setName('luigi')}>change name</button>
+            <p>{name}</p>
             {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs"/> */}
             {/* <h1>Home Page</h1> */}
             {/* <p>{name } is { age } years old</p> */}
