27ea20f6eb3ea90a9e0891970cfd3a843e4efe1b
ttran9 <toddtran9@gmail.com>
2021-02-04 00:16:03 -0800

    using custom hook to fetch data from db.

--- src/Home.js
+++ src/Home.js
@@ -1,42 +1,12 @@
 import React from "react";
-import { useState, useEffect } from 'react';
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-
-    /*
-     * this function below is run on the initial render and every time there is 
-     a re-rendering (such as when the data is changed).
-     * adding the empty array makes it so useEffect is only run once. 
-     * if we want useEffect to run when the name state changes but not when the blogs state changes
-     * then the name becomes a dependency we add into the second argument.
-     */
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:3000/blogs')
-                // get the response object and use the json function to get the response
-                .then(res => {
-                    if(!res.ok) {
-                        throw Error('could not fetch the data for that resource');
-                    }
-                    return res.json();
-                })
-                // grab from the data object.
-                .then((data) => {
-                    setBlogs(data);
-                    setIsPending(false);
-                    setError(null);
-                })
-                .catch((err) => {
-                    setError(err.message);
-                    setIsPending(false);
-                })
-        }, 1000)
-    }, []);
+    const url = 'http://localhost:3000/blogs';
+    // we are grabbing the data but call it "blogs" in our current context.
+    const { data:blogs, isPending, error } = useFetch(url);
 
     return ( 
         <div className="home">
