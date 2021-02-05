6a171066bfa617cde2ced4d29e169f793c632296
ttran9 <toddtran9@gmail.com>
2021-02-04 21:36:58 -0800

    using react router.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Home from "./Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+
 
 function App() {
-    const title = 'Welcome to the new blog';
-    const likes = 50;
-    // const person = { name: 'yoshi', age: 30};
-    const link = "http://www.google.com";
-
     return (
+        <Router>
             <div className="App">
                 <Navbar/>
                 <div className="content">
-                    <Home/>
+                    <Switch>
+                        <Route path="/">
+                            <Home/>
+                        </Route>
+                    </Switch>
                 </div>
             </div>  
+        </Router>
     )
 
 }
