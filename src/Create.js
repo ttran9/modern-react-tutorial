85ded6341f62f8203936c380fd26ac0cbe10fcf1
ttran9 <toddtran9@gmail.com>
2021-02-04 23:29:35 -0800

    redirecting user to home page after blog creation.

--- src/Create.js
+++ src/Create.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
 
@@ -6,6 +7,7 @@ const Create = () => {
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
     const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -22,7 +24,9 @@ const Create = () => {
         }).then(() => {
             console.log('new blog created!');
             setIsPending(false);
+            history.push("/");
         });
+
     }
 
     return (
